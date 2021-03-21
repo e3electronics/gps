c671f15b3b3b9b356afda1de30b7bd040414ca9d
Artemio <artemioev@gmail.com>
2021-03-20 21:28:25 -0400

    update

--- src/gps.c
+++ src/gps.c
@@ -48,11 +48,10 @@ static void parseGpsData(char *line)
 {
     char lineNmea[MINMEA_MAX_LENGTH];
     strncpy(lineNmea, line, sizeof(lineNmea) - 1);
-    // strcat(lineNmea, "\n");
-    // lineNmea[sizeof(lineNmea) - 1] = '\0';
-    // enum minmea_sentence_id id = minmea_sentence_id(lineNmea, false);
-    strncpy(lineNmea, line, sizeof(lineNmea));
+    strcat(lineNmea, "\n");
+    lineNmea[sizeof(lineNmea) - 1] = '\0';
     enum minmea_sentence_id id = minmea_sentence_id(lineNmea, false);
+  
     printf("sentence id = %d from line %s\n", (int)id, lineNmea);
     switch (id)
     {
